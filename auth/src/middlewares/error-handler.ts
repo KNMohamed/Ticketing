c709eb1ce011736fdd3693d42c415820bfedfd8b
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2022-05-18 23:07:58 -0400

    Create abstract Error class, handle invalid routes

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (
   err: Error,
@@ -8,22 +7,8 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    return res.status(400).json({
-      errors: err.errors.map((error) => {
-        return { message: error.msg, field: error.param };
-      }),
-    });
-  }
-
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(500).json({
-      errors: [
-        {
-          message: err.reason || 'Database Connection Error',
-        },
-      ],
-    });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   console.log('Something went wrong', err);
